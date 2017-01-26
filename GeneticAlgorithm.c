b575c06a1296be0a8b6a064926442a91eb59375c
Joel Andrepont <joelandrepont1@gmail.com>
2017-01-25 23:56:09 -0600

    Add files via upload

--- GeneticAlgorithm.c
+++ GeneticAlgorithm.c
@@ -9,6 +9,7 @@ char* variable[200];
 //void inst_Solution(int, int, int*);
 //int binary2Decimal(int*, int, int);
 float boundVariable( int );
+//int maxGenerations = 10;
 
 
 /*
@@ -31,12 +32,12 @@ typedef struct Generation {
 /*
  * Asks user for values used for genetic algorithm
 */
-float input_value(char* variable){
+float input_value(char* string){
 	
 	float value; 
-	printf("\n Enter a value for %s: \n", variable);
+	printf("\n Enter a value for %s: \n", string);
 	scanf("%f", &value);
-	printf("You entered: %f, for %s \n", value, variable);
+	printf("You entered: %f, for %s \n", value, string);
 	return value;
 }
 
@@ -49,7 +50,7 @@ float input_value(char* variable){
  *
 */
 
-void inst_Solution(int popLength, int soluLength, int solution[popLength][soluLength]){
+void inst_Solution(int popLength, int soluLength, struct Generation generation[], int generationNumber){
 
 	srand ( time(NULL) );
 	
@@ -57,7 +58,7 @@ void inst_Solution(int popLength, int soluLength, int solution[popLength][soluLe
 		
 		for(int j = 0; j < soluLength; j++){
 
-			solution[i][j] = rand() % 2;	
+			generation[generationNumber].population[i].chromosome[j] = rand() % 2;	
 		}
 	}
 
@@ -71,52 +72,39 @@ void inst_Solution(int popLength, int soluLength, int solution[popLength][soluLe
 * 
 */
 
-int binary2Decimal(int popLength, int soluLength, int solution[popLength][soluLength], int start, int end, int row){
+int binary2Decimal(int popLength, int soluLength, struct Generation* generation,
+	int start, int end, int row, int generationNumber){
 	int variableValue = 0;  //this will act as a sum value where all the binary values will be added together
 	int bitValue = 0;	//this acts as the sum value for a particular bit value
 	int length = end - start;
-	//int relativeStart = length % start;
-	//for( int row = 0; row < popLength; row++){
 		for( ; start < end; start++){
-			int bit = solution[row][start];
+			int bit = generation[generationNumber].population[row].chromosome[start];
+			//int bit = solution[row][start];
 			int relativeStart = start % length;
-			//printf("relativeStart = %d \n", relativeStart);
-			//printf("%d \n", variableValue);	
+			
 			if(bit == 1){ //only do something if the binary value is 1, not 0
 				if(relativeStart == 0){
-//					printf("printing value in if start == 0, before adding to, tha value is: %d \n", variableValue);
 					variableValue ++; //if the binary value is 1 and in the 0th position, add one to the variableValue
-//					printf("printing value in if start == 0, after adding to, tha value is: %d \n", variableValue);
 				}
 				if(relativeStart != 0){
 					if(bit == 1){
 						for(int x = 0; x <= relativeStart; x++){
 							if(x == 0){
-//								printf("printing value in if == 0, before adding to, the value is: %d \n", variableValue);
 								bitValue++;
-//								printf("printing value in if x == 0, after adding to, the value is: %d \n", variableValue);
 							}
 						
 							if(x != 0){
 						
-//								printf("printing bitValue in if x != 0, before adding to, the value is: %d \n", bitValue);
 								bitValue = 2*bitValue;
-//								printf("printing bitValue in if x != 0, after adding to, the value is: %d \n", bitValue);
 							}
-						//variableValue += bitValue;
 						}
-						//variableValue += bitValue;
 					}
 				variableValue += bitValue;
 				bitValue = 0;	 
 				}
-			//variableValue += bitValue;
-			//bitValue = 0;
 			}	
 		}
 		return variableValue;
-	//}		
-	//return variableValue;
 }
 
 
@@ -157,6 +145,69 @@ double functionCall(int popLength, int arrayLength, float boundedVariableValues[
 }
 
 
+void sort(Generation* generation, int generationNumber, int popLength){		
+	struct Population swap;
+	for (int c = 0 ; c < ( popLength - 1 ); c++){
+		for (int d = 0 ; d < popLength - c - 1; d++){
+
+			if (generation[generationNumber].population[d].fitnessScore < generation[generationNumber].population[d+1].fitnessScore) {
+				swap = generation[generationNumber].population[d];
+				generation[generationNumber].population[d] = generation[generationNumber].population[d+1];
+				generation[generationNumber].population[d+1] = swap;
+			}
+		}
+	}
+}
+
+void preserveElites(Generation* generation, int generationNumber, int eliteRate){ 
+	for(int n = 0; n < eliteRate; n++){
+		generation[generationNumber].population[n] = generation[generationNumber - 1].population[n];
+	}  
+}
+
+void preserveWorst(Generation* generation, int generationNumber, int popLength, int bottomRate){
+	for(; bottomRate > 0; bottomRate --){
+		int current = popLength - bottomRate;
+		generation[generationNumber].population[current] = generation[generationNumber - 1].population[current];
+	}
+}
+
+void mutation(Generation* generation, int generationNumber, int chromLength, 
+				int popLength, int mutation, int eliteRate, int bottomRate){
+	int modLength = popLength - (eliteRate + bottomRate);
+	srand(time(NULL));
+		
+	/*Need 
+	* To
+	* Fix 
+	* This
+	* Soon
+	*/
+	
+	int randomPop = rand() % modLength;
+	int randomGene = rand() % chromLength;
+	int bitValue = generation[generationNumber].population[randomPop].chromosome[randGene];
+	bitValue += 1;
+	bitValue = bitValue % 2;
+	generation[generationNumber].population[randomPop].chromosome[randGene] = bitValue;	
+}
+
+void cross(Generation generaton, int generationNumber, int chromLength, int popLength, 
+			int mutationRate, int eliteRate, int bottomRate){
+	srand(time(NULL));
+	for(n = 0; n < mutationRate; n++){
+		int randomPop = rand() % modLength;
+		int randomCross = rand() % 
+
+
+	}
+	//make it to where if x < bottomRate then x = x + bottomRate
+	// & where if x > popLength - eliteRate then x = x - eliteRate
+
+
+
+
+} 
 
 
 
@@ -166,8 +217,8 @@ double functionCall(int popLength, int arrayLength, float boundedVariableValues[
 
 
 int main(int argc, char* argv[0]){  //command line arguments
-	
-	float populationSize, chromosomeLength, variables, maxGenerations, eliteRate, crossOverRate, mutation, bottomRate;
+			
+	int populationSize, chromosomeLength, variables, maxGenerations, eliteRate, crossOverRate, mutation, bottomRate;
 	//Start and input values
 	//setVariables();
 
@@ -178,7 +229,7 @@ int main(int argc, char* argv[0]){  //command line arguments
 	 *
 	*/
 	
-	printf("All values must be entered as decimal values. Where 5 percent is 0.05.");
+	printf("All values must be entered as integer values. Where 5 percent is 5.");
 	populationSize = input_value("population Size");
 	chromosomeLength = input_value("Chromosome Length");
 	variables = input_value("number of variables");
@@ -189,134 +240,65 @@ int main(int argc, char* argv[0]){  //command line arguments
 	/*
 	 * Instantiate array for solution representation and fill with random bit values, make a 2d array 
 	*/
-	int soluLength = (int)chromosomeLength;	
-	int popLength = (int)populationSize;
-	int solution [popLength+1] [soluLength]; //popLength + 1 b/c will add fitness values later
-	inst_Solution(popLength, soluLength, solution);
+	int soluLength = chromosomeLength;	
+	int popLength = populationSize;
+	struct Generation generation[2];
 	
-	for(int n = 0; n < popLength; n++){
-		for(int k = 0; k < soluLength; k++){
-		printf("For row %d the value is %d \n", n, solution[n][k]);
+	for(int generationNumber = 0; generationNumber < maxGenerations; generationNumber ++){	
+		//instantiate
+		if(generationNumber == 0){
+			inst_Solution(popLength, soluLength, generation, generationNumber);
 		}
-	}	
 	
-
+		else{
+			preserveElites(generation , generationNumber, eliteRate);
+			preserveWorst(generation , generationNumber, popLength, bottomRate);
+			mutation(generation, generationNumber, chromLength, popLength, mutation, eliteRate, bottomRate);
+			cross(generaton, generationNumber, chromLength, popLength, mutation, eliteRate, bottomRate);	
+		//cross();
+		
 	
-	printf("variables = %f \n", variables);
-	int arrayLength = (int)variables;		
-	int unboundedVariableValues[popLength][arrayLength];
-	//1st need to convert into decimal and store in an array
-	//2nd bound the variables
-	//3rd create an array of the variable values
-	//1st
-	//for chrom length 16, 2 variables
-	int start = 0;
-	int variableLength = chromosomeLength / variables;
-	printf("variableLength = %d \n", variableLength);
-	int end = start + variableLength;
-	for (int row = 0; row < popLength; row++){
-		for(int n = 0; start < chromosomeLength; start = start + variableLength ){
-			unboundedVariableValues[row][n] = binary2Decimal(popLength, soluLength, solution, start, end, row);
-			n++;
-			end = end + variableLength;
-		}	
-		start = 0;
-		end = variableLength;	
-	}
-	/*
-	for(int i = 0; i < popLength; i++){
-		for(int j = 0; j < variables; j++){
-			printf("unboundedVariables Values for row %d = %d \n", i , unboundedVariableValues[i][j]);
+				
+		//get variable values
+		int arrayLength = variables;		
+		int unboundedVariableValues[popLength][arrayLength];
+		int start = 0;
+		int variableLength = chromosomeLength / variables;
+		int end = start + variableLength;	
+		for (int row = 0; row < popLength; row++){
+			for(int n = 0; start < chromosomeLength; start = start + variableLength ){
+				unboundedVariableValues[row][n] = binary2Decimal(popLength, soluLength, generation, start, end, row, generationNumber);
+				n++;
+				end = end + variableLength;
+			}	
+			start = 0;
+			end = variableLength;	
 		}
- 	}
-	*/
-	//2nd & 3rd
-	//Need to bound the variables
-	//then create 2d array with all the variable values. 
-	float boundedVariableValues[popLength][arrayLength];
-	for(int row = 0; row < popLength; row++){
-		for(int column = 0; column < arrayLength; column++){
-			int unboundedValue = unboundedVariableValues[row][column];
-			boundedVariableValues[row][column] = boundVariable( unboundedValue );
+		float boundedVariableValues[popLength][arrayLength];
+		for(int row = 0; row < popLength; row++){
+			for(int column = 0; column < arrayLength; column++){
+				int unboundedValue = unboundedVariableValues[row][column];
+				boundedVariableValues[row][column] = boundVariable( unboundedValue );
+			}
 		}
-	}
-	
-	/*
-	 *Check to see if values are bounded
-	for(int row = 0; row < popLength; row++){
-		for(int column = 0; column < arrayLength; column++){
-			printf("Bounded Variable for row %d = %f \n", row, boundedVariableValues[row][column]);
+		
+		//Now need to test the bounded variables and get a fitness score for each one. 
+		//double fitnessScores[popLength];
+		for(int row = 0; row < popLength; row++){
+			//int solution [popLength + 1] [soluLength];
+			//fitnessScores[row] = functionCall(popLength, arrayLength, boundedVariableValues, row);
+			generation[generationNumber].population[row].fitnessScore = functionCall(popLength, 
+																		arrayLength, boundedVariableValues, row);
 		}
-	}
-	*/
+		//sort the generation
+		sort(generation, generationNumber, popLength);
+		//populate the next generation
+		
 
-	//Now need to test the bounded variables and get a fitness score for each one. 
-	double fitnessScores[popLength];
-	for(int row = 0; row < popLength; row++){
-		//int solution [popLength + 1] [soluLength];
-		fitnessScores[row] = functionCall(popLength, arrayLength, boundedVariableValues, row);
-	}
-	/*
-	double swap;
-	double soluSwap[popLength];
-	double generationSorted[popLength+1][soluLength];
-	
-	for (int c = 0 ; c < ( popLength - 1 ); c++){
-    	for (int d = 0 ; d < popLength - c - 1; d++){
-
-      		if (fitnessScores[d] > fitnessScores[d+1])  For decreasing order use < {
-				swap = fitnessScores[d];
-				for(int i = 0; i < popLength; i++){
-					soluSwap[i] = solution[d][i];
-					solution[d][i] = solution[d+1][i];
-					solution[d+1][i] = soluSwap[i];
-				}
-        		fitnessScores[d]   = fitnessScores[d+1];
-        		fitnessScores[d+1] = swap;
-      		}
-    	}
-  	}
-
-	for(int i = 0; i < popLength; i++){
-		printf("The function returns %f for solution # %d \n", fitnessScores[i], i);
-	}
-	*/
-
-	struct Generation generation1;
-	for(int i = 0; i < popLength; i++){
-		for(int n = 0; n < soluLength; n++){
-			generation1.population[i].chromosome[n] = solution[i][n];
-			generation1.population[i].fitnessScore = fitnessScores[i];
+		
+		for(int i = 0; i < popLength; i++){
+			printf("Row [%d] has fitness score %f \n", i , generation[generationNumber].population[i].fitnessScore); 
 		}
-	}	
-	struct Population swap;
-	for (int c = 0 ; c < ( popLength - 1 ); c++){
-    	for (int d = 0 ; d < popLength - c - 1; d++){
-
-      		if (generation1.population[d].fitnessScore < generation1.population[d+1].fitnessScore)  /*For decreasing order use <*/ {
-				swap = generation1.population[d];
-				/*for(int i = 0; i < popLength; i++){
-					soluSwap[i] = solution[d][i];
-					solution[d][i] = solution[d+1][i];
-					solution[d+1][i] = soluSwap[i];
-				}*/
-				generation1.population[d] = generation1.population[d+1];
-        		//fitnessScores[d]   = fitnessScores[d+1];
-        		generation1.population[d+1] = swap;
-				//fitnessScores[d+1] = swap;
-      		}
-    	}
-  	}
-
-
-
-
-
-	for(int i = 0; i < popLength; i++){
-		printf("Row [%d] has fitness score %f \n", i , generation1.population[i].fitnessScore); 
-	}
-	for(int i = 0; i < soluLength; i++){
-	printf("%d", generation1.population[0].chromosome[i]);
 	}
 
 	return 0;
@@ -335,32 +317,3 @@ int main(int argc, char* argv[0]){  //command line arguments
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-	//for(int i = 0; i < maxGenerations; i++){
-		
-	//Intialization();
-	//all methods are working for initialization
-	//order for SGA:
-	//Initialization()
-		//this will encompass getting the variable values, and bounding them
-	//Fitness-Computation()
-		//compute fitness using function call and organize a 2d data structure based on this computation
-	//Sort()
-		//organize the 2d structure of the fitness scores
-	//Elite();
-	//Crossover();
-	//Weak-Elite();
-	//Mutation()
-	//Then loop back over.
