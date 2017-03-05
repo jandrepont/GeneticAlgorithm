9cf74fc8b81acd0db830af15b1115ee94f155f29
Joel Andrepont <joelandrepont1@gmailcom>
2017-03-04 20:37:35 -0600

    update

--- GeneticAlgorithm.c
+++ GeneticAlgorithm.c
@@ -3,6 +3,7 @@
 #include <time.h>
 #include <math.h>
 
+	
 //char variable[100];
 //float input_value(char*);
 char* string[200];
@@ -22,10 +23,12 @@ float boundVariable( int );
 typedef struct Population {
 	int chromosome[160];
 	double fitnessScore;
+    double similarity;
 } Population;
 
 typedef struct Generation {
 	struct Population population[200];
+    double similarity;
 } Generation;
 
 
@@ -52,7 +55,6 @@ float input_value(char* string){
 
 void inst_Solution(int popLength, int soluLength, struct Generation generation[], int generationNumber){
 
-	srand ( time(NULL) );
 	
 	for(int i = 0; i < popLength; i++){
 		
@@ -199,59 +201,26 @@ int rouletteWheel (Generation* generation, int previousGeneration, int popLength
 }
 
 
-void cross(Generation* generation, int generationNumber, int chromLength, int popLength, int mutationRate, int eliteRate, int crossRate){
+
+void cross1(Generation* generation, int generationNumber, int chromLength, int popLength, int mutationRate, int eliteRate, int crossRate){
     struct Generation temp;
     int numOfChildren = crossRate * popLength;
     int numOfElite = eliteRate * popLength;
     int start = numOfElite;
     int end = numOfElite + numOfChildren;
+    int previousGeneration = generationNumber - 1;
    // int numOfWorst = bottomRate * popLength;
    // int end = popLength - numOfWorst;
 
         
     for(start; start < end ; start  = start + 2){
         int geneStart = rand() % chromLength;
-       // int genestart2 = rand()% chromLength;
         int negGeneStart = geneStart;
-       // int negGeneStart2 = geneStart2;
-       // int previousGeneration = generationNumber - 1;
-        int randomPar1 = rand() % popLength;
-        int randomPar2 = rand() % popLength;
-       // int randomPar1 = rouletteWheel (generation, previousGeneration, popLength);
-       // int randomPar2 = rouletteWheel (generation, previousGeneration, popLength);
-        
-        
-        
-        int randGene = rand() % chromLength / 2;
-        int randGene2 = randGene + (rand() % chromLength / 2);
-        //go from o to randGene for both children
-     
-       
-        //offspring1 gets parent2 middle and parent1 exterior
-        //offspring2 gets parent1 middle and parent2 exterior
-        for(int start  = 0; start < randGene; start++){
-        
-            temp.population[0].chromosome[start] = generation[generationNumber].population[randomPar1].chromosome[start];
-            //temp.population[0].chromosome[geneStart] = generation[generationNumber].population[randomPar2].chromosome[negGeneStart];
-            
-            temp.population[1].chromosome[start] = generation[generationNumber].population[randomPar2].chromosome[start];
-            //temp.population[1].chromosome[negGeneStart] = generation[generationNumber].population[randomPar1].chromosome[negGeneStart];
-        }
-        for(int n = randGene2; n < chromLength; n++){
-            
-            temp.population[0].chromosome[n] = generation[generationNumber].population[randomPar1].chromosome[n];
-            temp.population[1].chromosome[n] = generation[generationNumber].population[randomPar2].chromosome[n];
-        }
-        for(randGene; randGene < randGene2; randGene++){
-
-            //notice how these two are switched, this way the middles are switched for the children. 
-            temp.population[1].chromosome[randGene] = generation[generationNumber].population[randomPar1].chromosome[randGene];
-            temp.population[0].chromosome[randGene] = generation[generationNumber].population[randomPar2].chromosome[randGene];
-
-        }
+        int randomPar1 = rouletteWheel(generation, previousGeneration, popLength);
+        int randomPar2 = rouletteWheel(generation, previousGeneration, popLength);
 
         //Single point crossover
-        /*
+        
         for(; geneStart < chromLength; geneStart++){
             temp.population[0].chromosome[geneStart] = generation[generationNumber].population[randomPar1].chromosome[geneStart];
             temp.population[0].chromosome[geneStart] = generation[generationNumber].population[randomPar2].chromosome[negGeneStart];
@@ -261,8 +230,103 @@ void cross(Generation* generation, int generationNumber, int chromLength, int po
             
             negGeneStart--;
         }
-        */
+        generation[generationNumber].population[start] = temp.population[0];
+        generation[generationNumber].population[start + 1] = temp.population[1];
+	}
+} 
 
+void cross2(Generation* generation, int generationNumber, int chromLength, int popLength, int mutationRate, int eliteRate, int crossRate){
+    struct Generation temp;
+    int numOfChildren = crossRate * popLength;
+    int numOfElite = eliteRate * popLength;
+    int start = numOfElite;
+    int end = numOfElite + numOfChildren;
+    int previousGeneration = generationNumber -1;
+    // int numOfWorst = bottomRate * popLength;
+   // int end = popLength - numOfWorst;
+
+        
+    for(start; start < end ; start  = start + 2){
+        
+        int randomPar1 = rouletteWheel(generation, previousGeneration, popLength);
+        int randomPar2 = rouletteWheel(generation, previousGeneration, popLength);
+        
+        //Random 2 point crossover
+        /*
+        int rand1 = random() % chromLength;
+        int rand2 = random() % chromLength;
+        int point1, point2;
+        
+        if(rand1 >= rand2){
+            point2 = rand1;
+            point1 = rand2;
+        }
+        else if(rand1 <= rand2){
+            point1 = rand1;
+            point2 = rand2;
+        }
+        */
+       
+        //Two Point Crossover
+        int midPoint = chromLength / 2;
+        int point1 = midPoint / 2;
+        int point2 = midPoint + point1;
+        //go from o to point1 for both children
+        //offspring1 gets parent2 middle and parent1 exterior
+        //offspring2 gets parent1 middle and parent2 exterior
+        
+
+        for(int start  = 0; start < point1; start++){
+        
+            temp.population[0].chromosome[start] = generation[generationNumber-1].population[randomPar1].chromosome[start]; 
+            temp.population[1].chromosome[start] = generation[generationNumber-1].population[randomPar2].chromosome[start];
+        }
+        for(int n = point2; n < chromLength; n++){
+            
+            temp.population[0].chromosome[n] = generation[generationNumber-1].population[randomPar1].chromosome[n];
+            temp.population[1].chromosome[n] = generation[generationNumber-1].population[randomPar2].chromosome[n];
+        }
+        for(int i = point1; point1 < point2; i++){
+
+            //notice how these two are switched, this way the middles are switched for the children. 
+            temp.population[1].chromosome[i] = generation[generationNumber-1].population[randomPar1].chromosome[i];
+            temp.population[0].chromosome[i] = generation[generationNumber-1].population[randomPar2].chromosome[i];
+
+        }
+        
+        generation[generationNumber].population[start] = temp.population[0];
+        generation[generationNumber].population[start + 1] = temp.population[1];
+	}
+} 
+
+
+void alternatingCross(Generation* generation, int generationNumber, int chromLength, int popLength, int mutationRate, int eliteRate, int crossRate){
+    struct Generation temp;
+    int numOfChildren = crossRate * popLength;
+    int numOfElite = eliteRate * popLength;
+    int start = numOfElite;
+    int end = numOfElite + numOfChildren;
+    int previousGeneration = generationNumber -1;
+        
+    for(start; start < end ; start  = start + 2){
+
+        int randomPar1 = rouletteWheel(generation, previousGeneration, popLength);
+        int randomPar2 = rouletteWheel(generation, previousGeneration, popLength);
+        //alternating crossover
+        
+        for(int n = 0; n < chromLength; n++){
+            if(n % 2 == 0){
+             
+                temp.population[0].chromosome[n] = generation[generationNumber].population[randomPar1].chromosome[n];
+                temp.population[1].chromosome[n] = generation[generationNumber].population[randomPar2].chromosome[n];
+            }
+            if(n % 2 == 1){
+
+                //notice how the parents are swithed
+                temp.population[0].chromosome[n] = generation[generationNumber].population[randomPar2].chromosome[n];
+                temp.population[1].chromosome[n] = generation[generationNumber].population[randomPar1].chromosome[n];
+            }       
+        }
         generation[generationNumber].population[start] = temp.population[0];
         generation[generationNumber].population[start + 1] = temp.population[1];
 	}
@@ -305,23 +369,51 @@ void mutate(Generation* generation, int generationNumber, int chromLength, int p
     }
 }
 
+double testSimilarity(Generation* generation, int generationNumber, int chromLength, int popLength, int start){
+    
+    //make it a recursive call for n-1? 
+    int i = start;
+    double similarChromes = 0;
+    double simPercentage = 0;
+    for(; i < popLength; i++){
+        for(int j = 0; j < chromLength; j++){
+            if(generation[generationNumber].population[start].chromosome[j] == generation[generationNumber].population[i].chromosome[j]){
+                similarChromes = similarChromes + 1;
+            }    
+        }
+    }
+    int numOfComparisons = i - start;
+    return simPercentage = similarChromes/ (numOfComparisons*chromLength);
+}
 
 
 
+
+double totalSimilarity(Generation* generation, int generationNumber, int chromLength, int popLength){
+    
+    double totalPercentage = 0;
+    for(int start = 0; start < popLength; start++){
+        totalPercentage += testSimilarity(generation, generationNumber, chromLength, popLength, start);
+        generation[generationNumber].population[start].similarity = testSimilarity(generation, generationNumber, chromLength, popLength, start);
+    }
+    
+    return totalPercentage = totalPercentage / popLength;
+}
 
 int main(){
 			
+    srand ( time(NULL) );
 	int populationSize = 200;
     int chromosomeLength =160; 
     int variables = 10; 
     int maxGenerations = 50; 
     float eliteRate = .05;
-    float crossRate = 0.05; 
+    float crossRate = 0.8; 
     float mutation = 0.1;
    // float bottomRate = 0.8;
 	
    //Start and input values 
-    printf("Would you like to input own values? Enter y or n : \n ");
+   // printf("Would you like to input own values? Enter y or n : \n ");
     char choice[0];
     scanf("%c", choice);
     if(choice[0] == 'y'){
@@ -340,19 +432,10 @@ int main(){
 	*/
 	int soluLength = chromosomeLength;	
 	int popLength = populationSize;
-	
 
-    //student *p = (student *)malloc(sizeof(student));
-
-    //int *list;
-
-    //list  = (int *) malloc(N * sizeof(int));i
 
     struct Generation* generation = malloc(maxGenerations * sizeof(*generation));
     generation[maxGenerations];
-    //hashTable *table = malloc(N*sizeof(hashTable)); //allocate memory
-    //... use it
-    //    free(table); //free memory
 
 	for(int generationNumber = 0; generationNumber < maxGenerations; generationNumber ++){	
 		//instantiate
@@ -362,14 +445,19 @@ int main(){
 	    //Since all new generation are built from old generations we pass
         //generationNumber -1
         //int previousGeneration = generationNumber - 1;
-
+        else if(generationNumber == (maxGenerations - 1)){ 
+			preserveElites(generation , generationNumber, eliteRate, popLength);
+			alternatingCross(generation, generationNumber, chromosomeLength, popLength, mutation, eliteRate, crossRate);
+            fillPop(generation, generationNumber, chromosomeLength, popLength, mutation, eliteRate, crossRate);
+			//shouldn't mutate on last generation
+            //mutate(generation, generationNumber, chromosomeLength, popLength, mutation, eliteRate);
+        }
 		else{
 			preserveElites(generation , generationNumber, eliteRate, popLength);
 			//preserveWorst(generation , generationNumber, popLength, bottomRate);
-			cross(generation, generationNumber, chromosomeLength, popLength, mutation, eliteRate, crossRate);
+			cross1(generation, generationNumber, chromosomeLength, popLength, mutation, eliteRate, crossRate);
             fillPop(generation, generationNumber, chromosomeLength, popLength, mutation, eliteRate, crossRate);
 			mutate(generation, generationNumber, chromosomeLength, popLength, mutation, eliteRate);
-            //cross();
 	    }	
 		
 		//get variable values
@@ -405,16 +493,13 @@ int main(){
 		}
 		//sort the generation
 		sort(generation, generationNumber, popLength);
-		//populate the next generation
-		
-
+		//test similarity
+        generation[generationNumber].similarity = totalSimilarity(generation, generationNumber, chromosomeLength, popLength);
+        
     
-      //  for(int i = 0; i < popLength; i++){
-      //      printf("Generation %d row [%d] has fitness score %f \n" , generationNumber , i,  generation[generationNumber].population[i].fitnessScore);
-      //  }
-    	
-	    printf("Generation %d has fitness score %f \n" , generationNumber , generation[generationNumber].population[0].fitnessScore); 
-      
+        printf("%d %f %f %f \n" , generationNumber, generation[generationNumber].population[0].fitnessScore, generation[generationNumber].population[0].similarity,
+                generation[generationNumber].similarity);     
+        //printf("Generation %d has similarity score for most fit pop[0]  %f \n" , generationNumber, generation[generationNumber].population[0].similarity); 
     }
 
     free(generation);
