d5f9c2bd08aea705e800944eb4b5ba63e4a9eb79
Joel Andrepont <joelandrepont1@gmailcom>
2017-03-23 15:40:46 -0500

    IT WORKSSSS

--- c++Dir/main.cpp
+++ c++Dir/main.cpp
@@ -14,8 +14,7 @@ void preserveElites(vector<Population> &population, int previousPop)
 	{
 		//cout << "\n pop[1] = " << population[1].get_fitness(n) << ", pop[0] = " << population[0].get_fitness(n) << "\n";
 
-		population[previousPop + 1].set_chrom(n, population[previousPop].get_chrom(n));
-		 
+		population[previousPop + 1].set_chrom(n, population[previousPop].get_chrom(n)); 
 		//cout << "\n After pop[1] = " << population[1].get_fitness(n) <<  ", pop[0] = " << population[0].get_fitness(n) << "\n";
 		i++;
 	}
@@ -39,49 +38,60 @@ int rouletteWheel(vector<Population> population, int prevPop)
 		random = random - current;
 		i++;
 	}
-
+	maxFitness = 0;
+	if(i == 200){
+		i--;
+	}
 	return i;
 }
 
 void cross1(vector<Population> &population, int popNum)
 {
-	int numOfChildren = population[popNum].get_cross() * population[popNum].get_numOfChrom();
+	int numOfChildren = 160;
+		//population[0].get_cross() * population[0].get_numOfChrom();
 		
 	
-	int start = population[popNum].get_elite() * population[popNum].get_numOfChrom();
+	int start = 20; 
+	//population[0].get_elite() * population[0].get_numOfChrom();
 	int previousPop = popNum - 1;
-	vector<Chromosome> chrom(2, Chromosome(160,10));
+	vector<Chromosome> chrom;
+	chrom.push_back(Chromosome(160,10));
+	chrom.push_back(Chromosome(160,10));
+
 	 	
 	for(int child = 0; child < numOfChildren; child+=2)
 	{
-		int geneStart = rand() % population[0].get_sizeOfGenes();
-		int negGeneStart = geneStart;
+		int geneStart = rand() % 160;
+			//population[0].get_sizeOfGenes();
+		int negGeneStart = geneStart - 1;
 		int randPar1 = rouletteWheel(population, previousPop);
 		int randPar2 = rouletteWheel(population, previousPop);
 		if(randPar1 == randPar2)
 		{
 			randPar2 = rouletteWheel(population, previousPop);
 
-		}
+		}	
 		
-		for(; geneStart < population[0].get_sizeOfGenes(); geneStart++)
+		for(int geneCount = 0; geneCount < 160 /* population[0].get_sizeOfGenes()*/  ; geneCount++)
 		{
 			
-			if(negGeneStart >= 0) //so we dont go out of bounds of the array. 
+			if(negGeneStart > 0) //so we dont go out of bounds of the array. 
 			{
 				
 				chrom[0].set_gene(negGeneStart, population[previousPop].get_gene(randPar2, negGeneStart)); 
 				chrom[1].set_gene(negGeneStart, population[previousPop].get_gene(randPar1, negGeneStart));
 				negGeneStart--;
 			}
-				
-			chrom[0].set_gene(geneStart, population[previousPop].get_gene(randPar1, geneStart));
+			if(geneStart < 160 /*population[0].get_sizeOfGenes() */){
+
+				chrom[0].set_gene(geneStart, population[previousPop].get_gene(randPar1, geneStart));
 																	//notice hows geneStart and negGeneStart swap
-			chrom[1].set_gene(geneStart, population[previousPop].get_gene(randPar2, geneStart));
-			
+				chrom[1].set_gene(geneStart, population[previousPop].get_gene(randPar2, geneStart));
+				geneStart++;
+			}
 		}
 		population[popNum].set_chrom(start, chrom[0]);
-		population[popNum].set_chrom(start + 1, chrom[1]);
+		population[popNum].set_chrom((start + 1), chrom[1]);
 		start += 2; 
 		
 	}
@@ -95,31 +105,38 @@ int main(){
 	//vector<Population> *population = new vector<Population>;
 	//std::vector<Population> population(50, Population(200, 160, 10));
 	std::vector<Population> population;
-	population.reserve(100);
+	population.reserve(1000);
+	population.push_back(Population(200,160,10));
+	population[0].set_elite(0.05);
+	population[0].set_cross(0.8);
 
-	
-	for(int i = 0; i < 100; i++)
-	{
-		
-		Population pop(200, 160, 10);
-		//population[i].set_popNum(i);
-		//population[i].set_cross(.8);
-		//population[i].set_elite(.05);
-		population.push_back( pop);
-
-	}
 
 	int populationNumber = 1;
 	int previousPopulation = 0;
-	for(int i = 0; i < 100; i++)
+	
+	int dummy = 0;
+
+	for(int i = 0; i < 1000; i++)
 	{
-		cout << "population = " << population[previousPopulation].get_fitness(0) << "\n";
+		
+		if(dummy > 0)
+		{
+			population.erase(population.begin());
+		}
+
+		population.push_back(Population(200, 160, 10));
+
+		cout << "population " << i << " = " <<  population[previousPopulation].get_fitness(0) << "\n";
+		
 		preserveElites(population, previousPopulation);
+		
 		cross1(population, populationNumber);
+		
 		population[populationNumber].mutate(10);
+		
 		population[populationNumber].sort();
-		populationNumber ++;
-		previousPopulation ++;
+		
+		dummy++;
 	}
 	
 
